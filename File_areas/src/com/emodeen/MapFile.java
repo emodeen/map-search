47f1854cd8ee872b8f0650fcc4fd46525fb19aea
Eric <emodeen@gmail.com>
2014-04-08 17:03:05 -0400

    Removed unused code.

--- File_areas/src/com/emodeen/MapFile.java
+++ File_areas/src/com/emodeen/MapFile.java
@@ -7,192 +7,33 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.URI;
 import java.util.*;
 
 /**
- * @author Eric
+ * @author Eric Modeen
  *
  */
 public class MapFile extends File {
 	
-	private List<Area> areas;
 	private Point[][] points;
 	private BufferedReader input;
 	int rootNode = 0;
 	private int maxArea;
 	
-	//boolean [] visited;
-	//int NNodes;
-	//int[][]  adjMatrix;
-
 	/**
 	 * @param pathname
 	 */
 	public MapFile( String path) {
+
 		super( path);
-		
-		
 	}
 
-
-	
-	/**
-	public void bfs()
-	{
-	      // BFS uses Queue data structure
-
-		Queue<Integer> q = new LinkedList<Integer>();
-
-	    q.add(rootNode);
-	    visited[rootNode] = true;
-
-	    //printNode(rootNode);
-
-	    while( !q.isEmpty() )
-	    {
-	       int n, child;
-
-	       n = (q.peek()).intValue();
-	       
-	       System.out.println("n= " + n);
-
-	       child = getUnvisitedChildNode(n);
-	       
-	       System.out.println("unvisited child node= " + child);
-
-	       if ( child != -1 )
-	       {
-	    	  // set to true when the x-coordinate equals 1 and has been visited.
-	          visited[child] = true;
-	          
-	          System.out.println("visited[" + child + "] set to true");
-
-	          //printNode(child);
-
-	          q.add(child);
-	       }
-	       else
-	       {
-	          q.remove();
-	       }
-	    }
-
-	       clearVisited();      //Clear visited property of nodes
-	   }
-*/
-
-	
-	   /**
-	    * 
-	    * @param n The y-coordinate
-	    * @return Returns the x-coordinate for all unvisited nodes that equal 1 for the passed in y-coordinate. 
-	    
-	   int getUnvisitedChildNode(int n)
-	   {
-	      int j;
-
-	      for ( j = 0; j < NNodes; j++ )
-	      {
-	    	  if ( adjMatrix[n][j] > 0 )
-	    	  {
-	    		  if ( ! visited[j] )
-	    			  return(j);
-	    	  }
-	      }
-
-	      return(-1);
-	   }
-	   */
-	
-	/**
-	 * This method returns a Point that is adjacent to the parent point and has not been visited.
-	 * @param node. The parent node.
-	 * @return
-	 
-	private Point getUnvisitedChildNode( Point parent) {
-		
-		 Point child = null;
-
-		 int n = parent.getyCoordinate();
-
-	     for (int j = 0; j < points.length; j++)
-	     {
-	    	 if ( points[n][j].getType() == CharType.CAPITAL_X)
-	         {
-	    		 if ( !points[n][j].isVisited())
-	    			 return child;
-	         	}
-	      	}
-
-	     return null;
-	}
-	
-	/**
-	 * 
-	 * @param p The Point to use as the root of the search.
-	 
-	private void breadthFirstSearch( Point rootNode) {
-		
-		// BFS uses Queue data structure
-		Queue queue = new LinkedList();
-		
-		queue.add( rootNode);
-		
-		// Add the point to the area.
-		printNode( rootNode.getxCoordinate() + "," + rootNode.getyCoordinate());
-		
-		rootNode.setVisited( true);
-		
-		while(!queue.isEmpty()) {
-		
-			// Retrieve the head node in the queue.
-			Point parent = (Point)queue.peek();
-			
-			Point child = null;
-			
-	        child = getUnvisitedChildNode( parent);
-
-	        if ( child != null)
-	        {
-	           child.setVisited( true);
-
-	   		   printNode( rootNode.getxCoordinate() + "," + rootNode.getyCoordinate());
-	           
-	           queue.add(child);
-	        }
-	     
-	        else
-	        {
-	           queue.remove();
-	        }
-	     }
-		
-		// Clear visited property of nodes
-		clearVisited();
-	}
-	*/
-	
-	   /**
-	void clearVisited()
-	{
-	   for (int i = 0; i < points.length; i++) {
-			
-		   for(int j=0; j < points[i].length; j++) {
-	      
-			   points[i][j].setVisited(false);
-		   }
-	   }
-	}
-	*/
-	   
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		
 		MapFile file = new MapFile( args[0]);
-		int numRows = 0;
 		
 		file.open();
 		
@@ -278,18 +119,9 @@ public class MapFile extends File {
 				points[p.getyCoordinate()][p.getxCoordinate()] = p;
 			}
 		}
-		
-		/*
-		for(int y=0; y < points.length; y++) {
-			for(int z=0; z < points[y].length; z++) {
-				points[y][z] = iterator.next();
-			}
-		}
-		*/
 	}
 	
 	/**
-	 * Later, I can re-factor this to use a List of Points.
 	 */
 	private void setMaxArea() {
 		
@@ -354,84 +186,6 @@ public class MapFile extends File {
 		
 		return areaSize;
 	}
-	
-
-	
-	/**
-	 * Traverse the file and store the points in the array of points.
-	 
-	private void read()
-	{
-		int rowNum = 0;
-
-		initializePointsArray();
-		
-		// Read the first row.
-		String row = readRow( rowNum);
-		
-		while ((row != null) && (!row.equals("")))
-		{
-			rowNum++;
-			row = readRow( rowNum);
-		}
-		
-		row = null;
-	}
-	
-	/**
-	 *
-	 * @return A row from the file.
-	 * Reads a row of the file, returning a Row object.
-	private String readRow( int rowNum)
-	{
-		String strRow = null;
-		CharType type = null;
-		
-		try {
-			
-			strRow = input.readLine();
-		}
-		
-		catch ( IOException e) {
-			System.out.println( "Error reading row");
-		}
-			
-
-		
-		strRow = null;
-		
-		return strRow;
-	}
-*/
-	
-	/**
-	 * Scan the file to find areas of dots.
-	 */
-	private void lookForAreas() {
-		
-		Area currentArea = null;
-
-		for(int i=0; i < points.length; i++) {
-			
-			for(int j=0; j < points[i].length; j++) {
-				
-				if( points[i][j].getType() == CharType.DOT) {
-					
-					// Check point to the NORTH, and add it to the area if needed.
-					if( (points[i][j-1] != null) && (points[i][j-1].getType() == CharType.DOT)) {
-						
-						if( currentArea == null) {
-							currentArea = new Area();
-						}
-						
-						currentArea.addPoint( points[i][j-1]);
-					}
-				}
-			}
-		}
-	}
-	
-
 	
 	private void printMaxArea() {
 		
--- File_areas/src/com/emodeen/Point.java
+++ File_areas/src/com/emodeen/Point.java
@@ -12,60 +12,10 @@ public class Point {
 	private int xCoordinate;
 	private int yCoordinate;
 	private CharType type;
-	/**
-	 * @param type the type to set
-	 */
-	public void setType(CharType type) {
-		this.type = type;
-	}
 
-
-	private Point leftPoint;
-	private Point rightPoint;
-	private Point abovePoint;
-	private Point belowPoint;
-	
-	// Whether the Point has been added to an area.
-	private boolean placedInArea;
-	
 	// Whether the point has been visited during the BFS.
 	private boolean visited;
 	
-	//private List surroundingPoints;
-	
-	
-	/**
-	 * @return the visited
-	 */
-	public boolean isVisited() {
-		return visited;
-	}
-
-
-	/**
-	 * @param visited the visited to set
-	 */
-	public void setVisited(boolean visited) {
-		this.visited = visited;
-	}
-
-
-	/**
-	 * @return the areaFound
-	 */
-	public boolean isPlacedInArea() {
-		return placedInArea;
-	}
-
-
-	/**
-	 * @param areaFound the areaFound to set
-	 */
-	public void setPlacedInArea(boolean areaFound) {
-		this.placedInArea = areaFound;
-	}
-
-
 	/**
 	 * @param xCoordinate
 	 * @param yCoordinate
@@ -77,29 +27,26 @@ public class Point {
 		this.type = type;
 		this.visited = false;
 	}
+	
+	/**
+	 * @return the visited
+	 */
+	public boolean isVisited() {
+		return visited;
+	}
 
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(CharType type) {
+		this.type = type;
+	}
 
-	void scanAdjacentPoints() {
-		
-		scanNorth();
-		
-	}
-	
-	private void scanNorth() {
-		
-		
-	}
-	
-	private void scanSouth() {
-		
-	}
-	
-	private void scanEast() {
-		
-	}
-	
-	private void scanWest() {
-		
+	/**
+	 * @param visited the visited to set
+	 */
+	public void setVisited(boolean visited) {
+		this.visited = visited;
 	}
 
 
